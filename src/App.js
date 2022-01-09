a166b63779c4e1542df9a56bcf41d0646a5b2089
Abdulbosit-AiM <abdulbosit.melikuziev@gmail.com>
2022-01-09 23:58:48 +0500

    add fetch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 const api = {
   key: '160a2a6a225cd6bb6f054adeb461ebbd',
   base: 'api.openweathermap.org/data/2.5/'
 }
 
 function App() {
+  const [query, setQuery] = useState('')
+  const [weather, setWeather] = useState({})
+
+  const search = evt => {
+    if (evt.key === "Enter"){ 
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then(res => res.json())
+        .then(result => setWeather(result))
+    }
+  }
   const dateBuilder = (d) => {
     let months = ['January', 'February','March','April','May','June','July','August','September','October','November','December']
     let days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
